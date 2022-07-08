c3bb3e5d3d3f3a69150ea49ed4ea0fd30f5e2a22
HenriqueMorato <riquemorato@gmail.com>
2022-07-07 21:14:20 -0300

    RentalPresenter com SimpleDelegator

--- app/presenters/rental_presenter.rb
+++ app/presenters/rental_presenter.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
-class RentalPresenter
-  def initialize(rental)
-    @rental = rental
-  end
-
+class RentalPresenter < SimpleDelegator
   def status
-    return '' unless rental.status
+    return '' unless super
 
-    color_class = STATUS_COLOR[rental.status.to_sym]
-    content_tag :span, t(rental.status), class: "badge bg-#{color_class}"
+    color_class = STATUS_COLOR[super.to_sym]
+    content_tag :span, t(super), class: "badge bg-#{color_class}"
   end
 
   private
@@ -27,13 +23,5 @@ class RentalPresenter
 
   def helpers
     ApplicationController.helpers
-  end
-
-  def method_missing(symbol, *args, &block)
-    rental.public_send(symbol, *args, &block)
-  end
-
-  def respond_to_missing?(method, include_all = false)
-    rental.respond_to?(method, include_all)
   end
 end
